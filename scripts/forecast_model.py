bc7c4526932141a17759429283429b9905d0b0ce
atyl386 <68893733+atyl386@users.noreply.github.com>
2021-02-12 11:47:10 +1300

    Add files via upload

--- scripts/forecast_model.py
+++ scripts/forecast_model.py
@@ -1,6 +1,6 @@
 import os, sys
 sys.path.insert(0, os.path.abspath('..'))
-from whakaari import TremorData, ForecastModel, load_dataframe
+from whakaari import TremorData, ForecastModel, load_dataframe, save_dataframe, datetimeify
 from datetime import timedelta, datetime
 
 # tsfresh and sklearn dump a lot of warnings - these are switched off below, but should be
@@ -14,18 +14,19 @@ warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=FitFailedWarning)
 
-
 def forecast_dec2019():
     ''' forecast model for Dec 2019 eruption
     '''
     # constants
+    threshold = 0.85
     month = timedelta(days=365.25/12)
     day = timedelta(days=1)
     td = TremorData()
-        
+    ti = '2011-01-01'
+    tf = '2013-01-01'
     # construct model object
     data_streams = ['rsam','mf','hf','dsar']
-    fm = ForecastModel(ti='2011-01-01', tf='2020-01-01', window=2., overlap=0.75, 
+    fm = ForecastModel(ti=ti, tf=tf, window=2., overlap=0.75,
         look_forward=2., data_streams=data_streams)
     
     # columns to manually drop from feature matrix because they are highly correlated to other 
@@ -33,95 +34,26 @@ def forecast_dec2019():
     drop_features = ['linear_trend_timewise','agg_linear_trend']
     
     # set the available CPUs higher or lower as appropriate
-    n_jobs = 6
-
+    n_jobs = 8
     # train the model, excluding 2019 eruption
-    # note: building the feature matrix may take several hours, but only has to be done once 
+    # note: building the feature matrix may take several hours, but only has to be done once
     # and will intermittantly save progress in ../features/
     # trained scikit-learn models will be saved to ../models/*root*/
-    te = td.tes[-1]
-    fm.train(ti='2011-01-01', tf='2020-01-01', drop_features=drop_features, retrain=False, 
-        exclude_dates=[[te-month,te+month],], n_jobs=n_jobs)      
+    fm.train(ti=ti, tf=tf, drop_features=drop_features, retrain=True, n_jobs=n_jobs)
 
     # run forecast from 2011 to 2020
     # model predictions will be saved to ../predictions/*root*/ 
-    ys = fm.forecast(ti='2011-01-01', tf='2020-01-01', recalculate=True, n_jobs=n_jobs)    
-
+    ys = fm.forecast(ti=ti, tf=tf, recalculate=True, n_jobs=n_jobs)
+    #fm.plot_accuracy(ys, save='{:s}\\accuracy_'.format(fm.plotdir)+'.png')
     # plot forecast and quality metrics
     # plots will be saved to ../plots/*root*/
-    fm.plot_forecast(ys, threshold=0.8, xlim = [te-month/4., te+month/15.], 
-        save=r'{:s}/forecast.png'.format(fm.plotdir))
-    fm.plot_accuracy(ys, save=r'{:s}/accuracy.png'.format(fm.plotdir))
-
+    fm.plot_forecast(ys, threshold=threshold, save='{:s}\\forecast_'.format(fm.plotdir)+'.png',xlim=['2011-01-01',tf])
     # construct a high resolution forecast (10 min updates) around the Dec 2019 eruption
     # note: building the feature matrix might take a while
-    fm.hires_forecast(ti=te-fm.dtw-fm.dtf, tf=te+month/30, recalculate=True, 
-        save=r'{:s}/forecast_hires.png'.format(fm.plotdir), n_jobs=n_jobs)
-
-def forecast_test():
-    ''' test scale forecast model
-    '''
-    # constants
-    month = timedelta(days=365.25/12)
+    #fm.hires_forecast(ti=datetimeify('2020-09-01'), tf=datetimeify('2020-10-01'), recalculate=False, 
+        #save='{:s}\\forecast_hires_'.format(fm.plotdir)+'.png', n_jobs=n_jobs, threshold=threshold, root='2020-09')
         
-    # set up model
-    data_streams = ['rsam','mf','hf','dsar']
-    fm = ForecastModel(ti='2012-07-15', tf='2012-08-16', window=2., overlap=0.75, 
-        look_forward=2., data_streams=data_streams, root='test', savefile_type='pkl')
-    
-    # set the available CPUs higher or lower as appropriate
-    n_jobs = 1
-    
-    # train the model
-    drop_features = ['linear_trend_timewise','agg_linear_trend']
-    fm.train(ti='2012-07-15', tf='2012-08-16', drop_features=drop_features, retrain=False,
-        n_jobs=n_jobs)      
-
-    # plot a forecast for a future eruption
-    # tf = te+month/30
-    # fm.hires_forecast(ti=te-fm.dtw-fm.dtf, tf=tf, recalculate=True, 
-    #     save=r'{:s}/forecast_Aug2013.png'.format(fm.plotdir), n_jobs=n_jobs)
-
-    te = fm.data.tes[1]
-    y = load_dataframe(r'D:\code\whakaari\predictions\test_hires\DecisionTreeClassifier_0000.pkl')
-    tf = y.index[-1] + month/30./10.
-    fm.hires_forecast(ti=te-fm.dtw-fm.dtf, tf=tf, recalculate=False, 
-        save=r'{:s}/forecast_Aug2013.png'.format(fm.plotdir), n_jobs=n_jobs)
-
-def forecast_now():
-    ''' forecast model for present day 
-    '''
-    # constants
-    month = timedelta(days=365.25/12)
-    day = timedelta(days=1)
-        
-    # pull the latest data from GeoNet
-    td = TremorData()
-    td.update()
-
-    # model from 2011 to present day (td.tf)
-    data_streams = ['rsam','mf','hf','dsar']
-    fm = ForecastModel(ti='2011-01-01', tf=td.tf, window=2, overlap=0.75,  
-        look_forward=2, data_streams=data_streams, root='online_forecaster')
-    
-    # set the available CPUs higher or lower as appropriate
-    n_jobs = 6
-    
-    # The online forecaster is trained using all eruptions in the dataset. It only
-    # needs to be trained once, or again after a new eruption.
-    # (Hint: feature matrices can be copied from other models to avoid long recalculations
-    # providing they have the same window length and data streams. Copy and rename 
-    # to *root*_features.csv)
-    drop_features = ['linear_trend_timewise','agg_linear_trend']
-    fm.train(ti='2011-01-01', tf='2020-01-01', drop_features=drop_features, 
-        retrain=True, n_jobs=n_jobs)      
-    
-    # forecast the last 7 days at high resolution
-    fm.hires_forecast(ti=fm.data.tf - 7*day, tf=fm.data.tf, recalculate=True, 
-        save='current_forecast.png', nztimezone=True, n_jobs=n_jobs)  
-
 if __name__ == "__main__":
-    #forecast_dec2019()
-    forecast_test()
+    forecast_dec2019()
+    #forecast_test()
     #forecast_now()
-    
